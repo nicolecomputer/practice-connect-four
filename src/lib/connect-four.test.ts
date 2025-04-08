1eae8193f98794a7f25025f20f30a359fdb2edac
Nicole <76132106+nicolecomputer@users.noreply.github.com>
2025-04-08 11:25:41 -0600

    add piece

--- src/lib/connect-four.test.ts
+++ src/lib/connect-four.test.ts
@@ -1,4 +1,4 @@
-import { emptyBoard } from "./connect-four";
+import { addPiece, emptyBoard, InvalidRowError, nextFreeSpot, RowFullError } from "./connect-four";
 import type { Board } from "./connect-four";
 
 describe("initialization", () => {
@@ -11,6 +11,80 @@ describe("initialization", () => {
             const result = emptyBoard(2, 2)
 
             expect(result).toEqual(expected)
+        })
+    })
+})
+
+describe("Add Pieces to the board", () => {
+    describe("nextFreeSpot", () => {
+        it("in an empty board the next free spot is 0", () => {
+            const expected = 1;
+            const board = emptyBoard(2, 2)
+            const result = nextFreeSpot(board, 0)
+
+            expect(result).toEqual(expected)
+        })
+
+        it("in a row with 1 piece, the next free spot is 1", () => {
+            const expected = 0;
+
+            const board: Board = [
+                [{ type: "empty-spot" }, { type: "filled-spot", color: 'red' }],
+                [{ type: "empty-spot" }, { type: "empty-spot" }]
+            ]
+            const result = nextFreeSpot(board, 0)
+
+            expect(result).toEqual(expected)
+        })
+
+        it("in a filled row it returns -1", () => {
+            const expected = -1;
+
+            const board: Board = [
+                [{ type: "filled-spot", color: 'blue' }, { type: "filled-spot", color: 'red' }],
+                [{ type: "empty-spot" }, { type: "empty-spot" }]
+            ]
+            const result = nextFreeSpot(board, 0)
+
+            expect(result).toEqual(expected)
+        })
+    })
+
+    it('adds a piece to an empty board', () => {
+        const expected: Board = [
+            [{ type: "empty-spot" }, { type: "empty-spot" }],
+            [{ type: "empty-spot" }, { type: "filled-spot", color: "red" }]
+        ]
+
+        let board = emptyBoard(2, 2)
+        board = addPiece(board, "red", 1)
+
+        expect(board).toEqual(expected)
+    })
+
+    describe("error states", () => {
+        it("throws an error when the row is full", () => {
+            const board: Board = [
+                [{ type: "filled-spot", color: 'blue' }, { type: "filled-spot", color: 'red' }],
+                [{ type: "empty-spot" }, { type: "empty-spot" }]
+            ]
+
+            expect(() =>
+                addPiece(board, "red", 0)
+            ).toThrow(RowFullError);
+        })
+        it("throws an error when the row is less than 0", () => {
+            const board = emptyBoard(2, 2)
+            expect(() =>
+                addPiece(board, "red", -1)
+            ).toThrow(InvalidRowError);
+        })
+
+        it('throws an error when the row is outside the board', () => {
+            const board = emptyBoard(2, 2)
+            expect(() =>
+                addPiece(board, "red", 2)
+            ).toThrow(InvalidRowError);
         })
     })
 })
--- src/lib/connect-four.ts
+++ src/lib/connect-four.ts
@@ -16,6 +16,8 @@ export type Row = Spot[];
 export type Board = Row[];
 
 
+// Initialization
+
 function emptyRow(depthOfRow: number): Row {
     return new Array<Spot>(depthOfRow).fill({ type: "empty-spot" })
 }
@@ -23,3 +25,51 @@ function emptyRow(depthOfRow: number): Row {
 export function emptyBoard(numberOfRows: number, depthOfRows: number): Board {
     return new Array<Row>(numberOfRows).fill(emptyRow(depthOfRows))
 }
+
+// Add pieces to the board
+export function nextFreeSpot(board: Board, row: number): number {
+    const firstFilled = board[row].findIndex(spot => spot.type === "filled-spot")
+
+    if (firstFilled < 0) {
+        return board[row].length - 1
+    }
+    return firstFilled - 1
+}
+
+export class RowFullError extends Error {
+    constructor(message: string) {
+        super(message);
+        this.name = "ConnectFour.BoardFull";
+        Object.setPrototypeOf(this, RowFullError.prototype);
+    }
+}
+
+export class InvalidRowError extends Error {
+    constructor(message: string = "") {
+        super(message);
+        this.name = "ConnectFour.InvalidRow";
+        Object.setPrototypeOf(this, InvalidRowError.prototype);
+    }
+}
+
+export function addPiece(board: Board, color: Color, row: number): Board {
+    if (row < 0 || row > (board.length - 1)) {
+        throw new InvalidRowError()
+    }
+
+    const spotForPiece = nextFreeSpot(board, row)
+
+    if (spotForPiece < 0) {
+        throw new RowFullError(`Row ${row} is already full`)
+    }
+
+    const nextRow = [...board[row]]
+    nextRow[spotForPiece] = { type: "filled-spot", color: color }
+
+    const nextBoard = [...board]
+    nextBoard[row] = nextRow
+
+    return nextBoard
+}
+
+// Check game state
